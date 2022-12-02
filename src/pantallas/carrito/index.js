1c677878e550b16b146998d39bfaea5117aceee9
cirilo38 <pablo.eluniversoweb@gmail.com>
2022-12-02 00:37:48 -0300

    Instalación tabs navigation y creacion de nuevas pantallas

---
+++ src/pantallas/carrito/index.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const Carrito = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Carrito</Text>
+    </View>
+  );
+};
+
+export default Carrito;
---
+++ src/pantallas/ordenes/index.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+const Ordenes = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Ordenes</Text>
+    </View>
+  );
+};
+
+export default Ordenes;
